cdc89d00e0ad2472915e692bcedaa9302cea2085
m1yabin <maaaakun25@yahoo.co.jp>
2023-03-22 20:49:06 +0900

    feat: 空文字だった時にメッセージを表示

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>My ToDo App</h1>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" /><button>追加</button
+    ><button>完了済みを削除する</button>
+  </form>
+  <p v-if="todos.length === 0">ToDoがまだありません。</p>
+  <p v-else></p>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      <input type="checkbox" checked /><span>{{ todo.text }}</span>
+    </li>
+    <li v-for="champion in champions" :key="champion.id">
+      <input type="checkbox" checked /><span>{{ champion.text }}</span>
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+let id = 0;
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      newTodo: "",
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      if (!this.newTodo) {
+        return alert("文字を入れてください");
+      }
+      this.todos.push({ id: id++, text: this.newTodo });
+      this.newTodo = "";
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #eee;
+}
+
+.todo-done {
+  text-decoration: line-through;
+}
+.champion-done {
+  text-decoration: line-through;
 }
 </style>
